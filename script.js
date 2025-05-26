668d5fc56c128a2467be492b69eb77a947a013ed
Namandeep795 <namandeepjain274@gmail.com>
2025-05-26 15:22:04 +0530

    Initial commit with index, style and script

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the button by its ID
+const button = document.getElementById("myButton");
+
+// Add a click event listener
+button.addEventListener("click", function() {
+  alert("Button clicked! Hello from JavaScript!");
+});
